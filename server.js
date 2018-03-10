ed47d9d92a52981c3464d3518a582ad481dccaeb
Gokul Nair <gokulgnair94@gmail.com>
2018-03-11 02:14:28 +0530

    New Repo

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ restService.use(bodyParser.urlencoded({
 }));
 restService.use(bodyParser.json());
 
-restService.use(express.static(path.join(__dirname, '/src/')));
+//restService.use(express.static(path.join(__dirname, '/src/')));
+
+
+app.use(express.static(__dirname + '/dist/'));
+app.use('/src/assets', express.static(__dirname + '/src/'));
 
 restService.listen((process.env.PORT || 9000), function() {
   console.log("Server up and listening");
