b43ee90dff7660670c1f59de2efee714e83e7603
Gokul Nair <gokulgnair94@gmail.com>
2018-03-11 02:04:41 +0530

    New Repo

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const restService = express();
+
+var path = require('path');
+restService.use(bodyParser.urlencoded({
+    extended: true
+}));
+restService.use(bodyParser.json());
+
+restService.use(express.static(path.join(__dirname, '/src/')));
+
+restService.listen((process.env.PORT || 9000), function() {
+  console.log("Server up and listening");
+});
